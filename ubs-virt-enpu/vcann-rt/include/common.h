0a886c7e534250bfbd8217e25ad44a7bb6f46a9a
Victor <victor.gao@huawei.com>
2026-01-30 17:47:12 +0800

    feat: add configuration module

---
+++ ubs-virt-enpu/vcann-rt/include/common.h
@@ -0,0 +1,58 @@
+/*
+* Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+* ubs-virt-ovs is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+* See the Mulan PSL v2 for more details.
+*/
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include "log.h"
+
+#define MAX_LINE_LENGTH 256
+
+#define ENPU_SUCCESS 0
+#define ENPU_FAIL 1
+
+#define CHECK_RANGE_INT(val, min, max)                                                     \
+    do {                                                                                   \
+        if (((val) < (min)) || ((val) > (max))) {                                          \
+            LOG_ERROR("Failed to load param [%s]: [%d] must be in range of [%d] and [%d]", \
+                #val, (val), (min), (max));                                                \
+            return ENPU_FAIL;                                                              \
+        }                                                                                  \
+    } while (false)
+
+/// DO NOT PASS FUNCTION IN
+#define CHECK_ERROR_CODE(err, error_msg, ...)    \
+    do {                                         \
+        if ((err) != ENPU_SUCCESS) {             \
+            LOG_ERROR(error_msg, ##__VA_ARGS__); \
+            return (err);                        \
+        }                                        \
+    } while (false)
+
+/// DO NOT PASS FUNCTION IN
+#define CHECK_ERROR_CODE_NORETURN(err, error_msg, ...)    \
+    do {                                                  \
+        if ((err) != ENPU_SUCCESS) {                      \
+            LOG_ERROR(error_msg, ##__VA_ARGS__);          \
+        }                                                 \
+    } while (false)
+
+extern int get_config_value(const char *file_path, const char *key_name, char *buffer);
+#endif
---
+++ ubs-virt-enpu/vcann-rt/include/config.h
@@ -0,0 +1,39 @@
+/*
+* Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+* ubs-virt-ovs is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+* See the Mulan PSL v2 for more details.
+*/
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include "common.h"
+
+#define SHM_ID_LEN 128
+#define OPTION_NPU_ID "physical-npu-id"
+#define OPTION_VNPU_ID "virtual-npu-id"
+#define OPTION_AICORE_QUOTA "aicore-quota"
+#define OPTION_MEMORY_QUOTA "memory-quota"
+#define OPTION_SHM_ID "shm-id"
+#define OPTION_SCHEDULING_POLICY "scheduling-policy"
+#define INVALID_VALUE (-1)
+
+struct Config {
+    int32_t phy_npu_id;
+    int32_t vnpu_id;
+    int32_t aicore_quota;
+    int32_t memory_quota;
+    int32_t scheduling_policy;
+    char shm_id[SHM_ID_LEN];
+};
+
+extern struct Config config;
+
+int load_config(const char *file_path);
+
+#endif
