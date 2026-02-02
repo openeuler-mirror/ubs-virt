b5d8a86bec28359074f4c3e9e89cb605237fcace
miaoyxm <yexiangmin@huawei.com>
2026-02-02 20:26:49 +0800

    clean code

--- ubs-virt-enpu/vcann-rt/src/log.c
+++ ubs-virt-enpu/vcann-rt/src/log.c
@@ -107,7 +107,7 @@ int count_log_files()
             continue;
         }
         char full_path[FILE_PATH_LEN];
-        int ret = snprintf_s(full_path, sizeof(full_path), sizeof(full_path), "%s%s", 
+        int ret = snprintf_s(full_path, sizeof(full_path), sizeof(full_path), "%s%s",
             g_log_config.log_dir, entry->d_name);
         if (ret < 0) {
             if (closedir(dir) == -1) {
@@ -145,7 +145,7 @@ int compress_file()
         perror("Compress files error, failed to format timestamp");
         return ENPU_FAIL;
     }
-    int ret = snprintf_s(zip_file, sizeof(zip_file), sizeof(zip_file), "%s%s_%s", 
+    int ret = snprintf_s(zip_file, sizeof(zip_file), sizeof(zip_file), "%s%s_%s",
         g_log_config.log_dir, SUB_MODULE_NAME, timestamp);
     if (ret < 0) {
         return ENPU_FAIL;
@@ -155,7 +155,7 @@ int compress_file()
         return ENPU_FAIL;
     }
     umask(SET_UMASK_FOR_440); // 默认权限440
-    ret = snprintf_s(tar_cmd, sizeof(tar_cmd), sizeof(tar_cmd), "%s%s %s%s %s%s", TAR_CMD_PREFIX, zip_file, 
+    ret = snprintf_s(tar_cmd, sizeof(tar_cmd), sizeof(tar_cmd), "%s%s %s%s %s%s", TAR_CMD_PREFIX, zip_file,
         "--exclude=", g_log_config.log_path, g_log_config.log_dir, "*.log");
     if (ret < 0) {
         return ENPU_FAIL;
@@ -232,7 +232,7 @@ int log_init()
 
     printf("dir_path: %s\n", g_log_config.log_dir);
     char mkdir_cmd[MAX_CMD_LEN];
-    int ret = snprintf_s(mkdir_cmd, sizeof(mkdir_cmd), sizeof(mkdir_cmd), "%s%s", 
+    int ret = snprintf_s(mkdir_cmd, sizeof(mkdir_cmd), sizeof(mkdir_cmd), "%s%s",
         MKDIR_CMD_PREFIX, g_log_config.log_dir);
     if (ret < 0) {
         return ENPU_FAIL;
